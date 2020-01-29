12bce5fd309f70787c463d3cec75fd0e5c473b0d
gregsensem <pansen1990@gmail.com>
2020-01-28 21:48:25 -0500

    add AppLayout

---
+++ src/welcomeMessage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const WelcomeMessage = () => {
+    return ( 
+        <div>
+            <h1>Welcome to CryptoDash</h1>
+        </div>
+     );
+}
+ 
+export default WelcomeMessage;
