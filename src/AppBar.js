b4de9a3d088580d0cce01cd2b55cc1a8381b7d4f
gregsensem <pansen1990@gmail.com>
2020-01-29 12:07:52 -0500

    style AppBar

--- src/AppBar.js
+++ src/AppBar.js
@@ -1,18 +1,41 @@
 import React from 'react'
-import styled from 'styled-components';
+import styled, {css} from 'styled-components';
 
 const Bar = styled.div`
     display:grid
     grid-template-columns:200px auto 120px 120px;
+    margin-bottom:40px;
 `;
+
+const Logo =styled.div`
+    font-size:1.5rem;
+`;
+
+const ControlButtonElem = styled.div`
+    cursor:pointer;
+    ${props=>props.active && css`
+        text-shadow:0px 0px 60px #03ff03 
+    `}
+`;
+
+function toProperCase(string){
+    return string.charAt(0).toUpperCase() + string.substr(1);
+}
+
+function ControlButton({name,active}){
+    return(
+    <ControlButtonElem active={active}>{toProperCase(name)}</ControlButtonElem>
+    )
+}
+
 
 const AppBar = () => {
     return ( 
     <Bar>
-        <div>CryptoDash</div>
+        <Logo>CryptoDash</Logo>
         <div />
-        <div>Dashboard</div>
-        <div>Settings</div>
+        <ControlButton active name='dashbord' />
+        <ControlButton name='settings' />
     </Bar> );
 }
  
