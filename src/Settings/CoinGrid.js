4b8d0f1accae072fde365264e410ce6b86eb2534
gregsensem <pansen1990@gmail.com>
2020-02-01 19:24:47 -0500

    create CoinGrid

--- src/Settings/CoinGrid.js
+++ src/Settings/CoinGrid.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import styled,{css} from 'styled-components';
 import {AppContext} from '../AppProvider';
+import {SelectableTile} from "../Shared/Tile";
 
 export const CoinGridStyled = styled.div`
     display: grid;
     grid-template-columns: repeat(5,1fr);
+    grid-gap:15px;
 `
 
 export default function(){
     return <AppContext.Consumer>
         {({coinList})=><CoinGridStyled>
-            {Object.keys(coinList).map(coin=><div>{coin}</div>)}
+            {Object.keys(coinList).map(coin=><SelectableTile>{coin}</SelectableTile>)}
             </CoinGridStyled>}
     </AppContext.Consumer>
 }
--- src/Settings/confirmbutton.js
+++ src/Settings/confirmbutton.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import styled from 'styled-components';
 import {AppContext} from '../AppProvider';
+import {fontSize1, greenBoxShadow, color3} from "../Shared/styles";
 
 const ConfirmButtonStyled = styled.div`
     margin:20px;
-    color:green;
+    color:${color3}; 
+    ${fontSize1}
     cursor:pointer;
+    padding 5px;
+    &:hover{
+        ${greenBoxShadow}
+    }
 `
 export const CenterDiv = styled.div`
     display:grid;
