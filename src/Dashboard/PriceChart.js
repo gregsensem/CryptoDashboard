69ce4a227b39377889b4f132109a6eb3f46032f8
gregsensem <pansen1990@gmail.com>
2020-02-13 19:59:29 -0500

    add HighCharts Theme

--- src/Dashboard/PriceChart.js
+++ src/Dashboard/PriceChart.js
@@ -1,8 +1,10 @@
-import highchartsConfig from './HighchartsConfig';
 import React from 'react';
 import {Tile} from '../Shared/Tile';
 import { AppContext } from '../AppProvider';
 import ReactHighcharts from 'react-highcharts';
+import highchartsConfig from './HighchartsConfig';
+import HighchartsTheme from './HighchartsTheme';
+ReactHighcharts.Highcharts.setOptions(HighchartsTheme);
 
 export default function(){
     return(
